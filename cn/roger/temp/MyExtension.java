02bd633b471347ba243961752969c948d25a91e3
RogerYoungh <42564206+RogerYoungh@users.noreply.github.com>
2018-08-21 13:45:15 +0800

    Create MyExtension.java

---
+++ cn/roger/temp/MyExtension.java
@@ -0,0 +1,29 @@
+package cn.roger.temp;
+
+import com.google.appinventor.components.annotations.*;
+import com.google.appinventor.components.common.ComponentCategory;
+import com.google.appinventor.components.runtime.*;
+import com.google.appinventor.components.runtime.util.*;
+import com.google.appinventor.components.runtime.errors.YailRuntimeError;
+
+@DesignerComponent(version = MyExtension.VERSION,
+    description = "by Roger Young",
+    category = ComponentCategory.EXTENSION,
+    nonVisible = true,
+    iconName = "images/extension.png")
+
+@SimpleObject(external = true)
+
+public class MyExtension extends AndroidNonvisibleComponent {
+    public static final int VERSION = 1;
+    private static final String LOG_TAG = "Test";
+    
+    public MyExtension(ComponentContainer container) {
+        super(container.$form());
+    }
+    
+    @SimpleFunction(description = "add up a and b")
+    public String addab(int a,int b) {
+        return ""+(a+b);
+    }
+}
